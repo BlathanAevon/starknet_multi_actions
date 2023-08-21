eac5cfd79ad1dae7e8da700fbc88560d306a48dd
Blathan Aevon <bundemshake@gmail.com>
2023-08-21 06:58:24 +0200

    new file:   id_minter_abi.py 	modified:   main.py 	modified:   settings.py

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from starknet_py.net.full_node_client import FullNodeClient
 from starknet_py.net.models.chains import StarknetChainId
 from starknet_py.net.signer.stark_curve_signer import KeyPair
 from dmail_abi import DMAIL_TRANSACTION_ABI
+from id_minter_abi import MINT_ID_ABI
 from starknet_py.contract import Contract
 from loguru import logger
-from settings import ACCEPTABLE_GWEI, MESSAGES_PER_ACCOUNT_FROM, MESSAGES_PER_ACCOUNT_TO, M_DELAY_FROM, M_DELAY_TO, DELAY_FROM, DELAY_TO, STARKNET_NODE
+from settings import (
+    ACCEPTABLE_GWEI,
+    MESSAGES_PER_ACCOUNT_FROM,
+    MESSAGES_PER_ACCOUNT_TO,
+    M_DELAY_FROM,
+    M_DELAY_TO,
+    DELAY_FROM,
+    DELAY_TO,
+    STARKNET_NODE,
+    MODULES,
+    RANDOMIZE_MODULES,
+    MOD_DELAY_FROM,
+    MOD_DELAY_TO,
+)
 import asyncio
 import string
 import random
@@ -13,14 +27,15 @@ import csv
 import time
 from web3 import Web3
 
+
 def get_current_gas_price():
     try:
         # Connect to an Ethereum node using Web3
-        w3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
+        w3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/eth"))
 
         # Get the current gas price in GWEI
         gas_price_wei = w3.eth.gas_price
-        gas_price_gwei = w3.from_wei(gas_price_wei, 'gwei')
+        gas_price_gwei = w3.from_wei(gas_price_wei, "gwei")
 
         return round(gas_price_gwei, 0)
     except Exception as e:
@@ -55,15 +70,32 @@ async def dmail_send_email(account):
     logger.success(f"Transaction done!")
 
 
+async def mint_starknet_id(account):
+    contract = Contract(
+        address=0x05DBDEDC203E92749E2E746E2D40A768D966BD243DF04A6B712E222BC040A9AF,
+        abi=MINT_ID_ABI,
+        provider=account,
+    )
+
+    logger.info("Assembling Transaction...")
+    invocation = await contract.functions["mint"].invoke(
+        random.randint(400000, 20000000), auto_estimate=True
+    )
+
+    logger.info("Minting an ID...")
+    await invocation.wait_for_acceptance()
+    logger.success(f"Transaction done!")
+
+
 async def main():
     accounts = []
     with open("wallets.csv", newline="") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=",")
         for row in spamreader:
-            if str(row[1]).startswith("0x"):
-                private_key = int(row[1], 16)
+            if str(row[1].strip()).startswith("0x"):
+                private_key = int(row[1].strip(), 16)
             else:
-                private_key = int(row[1])
+                private_key = int(row[1].strip())
 
             address = row[0]
             starknet_account = Account(
@@ -96,21 +128,39 @@ async def main():
                     logger.success("Gas is acceptable for work")
                     break
 
-            tts = random.randint(DELAY_FROM, DELAY_TO)
-
             logger.info(f"Working on wallet: {hex(acc.address)}\n")
-            if MESSAGES_PER_ACCOUNT_FROM > 1:
-                for _ in range(random.randint(MESSAGES_PER_ACCOUNT_FROM, MESSAGES_PER_ACCOUNT_TO)):
-                    message_delay = random.randit(M_DELAY_FROM, M_DELAY_TO)
-                    await dmail_send_email(acc)
-                    logger.opt(colors=True).info(
-                        f"<yellow>Waiting {message_delay} before next message</yellow>"
-                    )
-                    time.sleep(message_delay)
-            else:
-                await dmail_send_email(acc)
 
-            logger.info(f"Sleeping {tts} seconds...\n")
+            if RANDOMIZE_MODULES:
+                random.shuffle(MODULES)
+                logger.warning("Modules randomized!")
+
+            for module in MODULES:
+                if module == "mint_starknet_id":
+                    await mint_starknet_id(acc)
+                elif module == "dmail_send_email":
+                    if MESSAGES_PER_ACCOUNT_FROM > 1:
+                        for _ in range(
+                            random.randint(
+                                MESSAGES_PER_ACCOUNT_FROM, MESSAGES_PER_ACCOUNT_TO
+                            )
+                        ):
+                            message_delay = random.randit(M_DELAY_FROM, M_DELAY_TO)
+                            await dmail_send_email(acc)
+                            logger.opt(colors=True).info(
+                                f"<yellow>Waiting {message_delay} before next message</yellow>"
+                            )
+                            time.sleep(message_delay)
+                    else:
+                        await dmail_send_email(acc)
+                else:
+                    logger.error("MODULE NAME ERROR, CHECK THE CODE!")
+
+                mod_tts = random.randint(MOD_DELAY_FROM, MOD_DELAY_TO)
+                logger.warning(f"Sleeping {tts} seconds between modules...\n")
+                time.sleep(mod_tts)
+
+            tts = random.randint(DELAY_FROM, DELAY_TO)
+            logger.info(f"Sleeping {tts} seconds before next wallet...\n")
             time.sleep(tts)
 
 
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ STARKNET_NODE = "https://starknet-mainnet.public.blastapi.io"
 
 # How many messages to send on every acccount
 MESSAGES_PER_ACCOUNT_FROM = 1
-MESSAGES_PER_ACCOUNT_TO = 2
+MESSAGES_PER_ACCOUNT_TO = 1
 
 # Delay between accounts
 DELAY_FROM = 40
@@ -12,5 +12,15 @@ DELAY_TO = 300
 M_DELAY_FROM = 10
 M_DELAY_TO = 60
 
+# Delay between modules
+MOD_DELAY_FROM = 20
+MOD_DELAY_TO = 80
+
 # Gas value at which script is acceptable to work
-ACCEPTABLE_GWEI = 16
+ACCEPTABLE_GWEI = 17
+
+# True if you want to randomize moudules execution
+RANDOMIZE_MODULES = True
+
+# Available modules: dmail_send_email, mint_starknet_id
+MODULES = ["dmail_send_email", "mint_starknet_id"]
